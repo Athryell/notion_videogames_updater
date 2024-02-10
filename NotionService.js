e9f574f0ae365cd699eb232819ce30a73f1db537
Athryell <93521016+Athryell@users.noreply.github.com>
2024-02-10 18:51:41 +0100

    Added getNotionEntryNames in NotionService

--- NotionService.js
+++ NotionService.js
@@ -27,3 +27,27 @@ export async function getAllNotionEntries() {
     console.log(pages.length);
     //return pages;
 }
+
+export async function getNotionEntryNames() {
+    let names = [];
+    let hasMorePages = true;
+    let startCursor = undefined;
+  
+    while (hasMorePages) {
+      const response = await notion.databases.query({
+        database_id: databaseId_NextGame,
+        start_cursor: startCursor,
+      });
+  
+      names = names.concat(response.results.map(entry => entry.properties["Name"].title[0] ? entry.properties["Name"].title[0].text.content : ""));
+  
+      if (response.has_more) {
+        startCursor = response.next_cursor;
+      } else {
+        hasMorePages = false;
+      }
+    }
+    
+    console.log(pages.length);
+    //return names;
+}
