5bf85883b568bce73f6f0d557abb2d9fcbabb996
Athryell <93521016+Athryell@users.noreply.github.com>
2024-02-10 18:54:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+import * as notionService from './notionServices.js';
+
 function showOnPage() {
     var notionOAuth = document.getElementById("notion-auth").value;
+    notionService.setOAuth(notionOAuth);
     var notionId = document.getElementById("notion-id").value;
+    notionService.setDatabaseId(notionId);
+    
     var score = document.getElementById("checkbox_score").checked;
     var time = document.getElementById("checkbox_time").checked;
     var language = document.getElementById("checkbox_language").checked;
@@ -9,5 +14,7 @@ function showOnPage() {
       selectedLanguage = "";
     }
     var checkbox4 = document.getElementById("checkbox_gamepass").checked;
-    document.getElementById("output").innerHTML = `${notionId} ${score} ${time} ${language} ${selectedLanguage}`;
+    //document.getElementById("output").innerHTML = `${notionId} ${score} ${time} ${language} ${selectedLanguage}`;
+
+    notionService.getNotionEntryNames();
 }
