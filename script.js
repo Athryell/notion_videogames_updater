a4f8398a5526ee25246dd3308b72a79f49ddd52d
Athryell <93521016+Athryell@users.noreply.github.com>
2024-02-10 18:34:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function showOnPage() {
-    var text = document.getElementById("myText").value;
-    var checkbox1 = document.getElementById("checkbox_score").checked;
-    var checkbox2 = document.getElementById("checkbox_time").checked;
-    var checkbox3 = document.getElementById("checkbox_language").checked;
-    var select = document.getElementById("select_language").value;
-    if(!checkbox3) {
-      select = "";
+    var notionOAuth = document.getElementById("notion-auth").value;
+    var notionId = document.getElementById("notion-id").value;
+    var score = document.getElementById("checkbox_score").checked;
+    var time = document.getElementById("checkbox_time").checked;
+    var language = document.getElementById("checkbox_language").checked;
+    var selectedLanguage = document.getElementById("select_language").value;
+    if(!language) {
+      selectedLanguage = "";
     }
     var checkbox4 = document.getElementById("checkbox_gamepass").checked;
-    document.getElementById("output").innerHTML = `${text} ${checkbox1} ${checkbox2} ${select} ${checkbox4}`;
+    document.getElementById("output").innerHTML = `${notionId} ${score} ${time} ${language} ${select}`;
 }
